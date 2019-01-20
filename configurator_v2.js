b6af3ed76c735e031372369c46be549c52d82744
aaronlhe <aaronlhe@outlook.com>
2019-01-20 22:05:36 +0800

    v2 push

--- configurator_v2.js
+++ configurator_v2.js
@@ -1,3 +1,27 @@
 /*
-Rewrite of logic by Aaron Lee
+EZBO Stacking Cube Product Configurator Web App v2
 */
+
+
+
+var initBaseCube= 1; // base cube initialization case
+
+// assign basecubes file name for auto import of mesh. 
+var bcubesName; // initial import of base cubes
+switch (baseCubeNum) {
+     case 1:
+          bcubesName = 'B1-final.babylon'; break;
+     case 2:
+          bcubesName = 'B2-final.babylon'; break;
+     case 3:
+          bcubesName = 'B3-final.babylon'; break; 
+     case 4:
+          bcubesName = 'B4-final.babylon'; break; 
+     case 5:
+          bcubesName = 'B5-final.babylon'; break; 
+     case 6:
+          bcubesName = 'B6-final.babylon'; 
+}
+
+
+
